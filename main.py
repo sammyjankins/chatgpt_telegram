03e899f7c6b2a7bace9a4a6f8756e3bb7229d24d
SammyJankins <neroragno@mail.ru>
2023-04-02 03:47:41 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,79 @@
+import logging
+
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update
+from telegram.ext import (
+    Application,
+    CommandHandler,
+    ContextTypes,
+    ConversationHandler,
+    MessageHandler,
+    filters,
+)
+
+from prompt_engine import eval_prompt
+from secrets import TG_TOKEN
+
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+logger = logging.getLogger(__name__)
+
+START_STATE, DIALOGUE_STATE, LOCATION, BIO = range(4)
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+
+    await update.message.reply_text(
+        "Hi! write me to start a conversation."
+    )
+
+    return START_STATE
+
+
+async def dialogue(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    request = update.message.text
+    dialogue_context = context.user_data.get('dialogue')
+    response = eval_prompt(request, dialogue_context)
+    if dialogue_context:
+        dialogue_context.append((request, response))
+        if len(dialogue_context) > 10:
+            dialogue_context = dialogue_context[1:]
+    else:
+        dialogue_context = [(request, response)]
+    context.user_data['dialogue'] = dialogue_context
+
+    await update.message.reply_text(
+        text=response
+    )
+
+    return DIALOGUE_STATE
+
+
+async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    user = update.message.from_user
+    logger.info("User %s canceled the conversation.", user.first_name)
+    await update.message.reply_text(
+        "Bye! I hope we can talk again some day.", reply_markup=ReplyKeyboardRemove()
+    )
+
+    return ConversationHandler.END
+
+
+def main() -> None:
+    application = Application.builder().token(TG_TOKEN).build()
+
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler("start", start)],
+        states={
+            START_STATE: [MessageHandler(filters.TEXT & ~filters.COMMAND, dialogue)],
+            DIALOGUE_STATE: [MessageHandler(filters.TEXT & ~filters.COMMAND, dialogue)],
+        },
+        fallbacks=[CommandHandler("cancel", cancel)],
+    )
+
+    application.add_handler(conv_handler)
+    application.run_polling()
+
+
+if __name__ == "__main__":
+    main()
---
+++ prompt_engine.py
@@ -0,0 +1,22 @@
+import openai
+from secrets import OPEN_AI_KEY
+
+api_endpoint = 'https://api.openai.com/v1/completions'
+
+openai.api_key = OPEN_AI_KEY
+
+
+def eval_prompt(request, context=None):
+    if context:
+        dialogue = ''.join([f"1: {item[0]}\n2: {item[1]}\n" for item in context])
+        prompt = f"continue the dialogue -\n{dialogue}1: {request}\n2: "
+    else:
+        prompt = request
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=prompt,
+        max_tokens=400,
+        temperature=0.8
+    )
+    return response['choices'][0]['text']
+
